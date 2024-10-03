d32b60ac659ce947762db15aa1251fe5b27252c7
EverardoG <everardo.a.gonzalez@gmail.com>
2024-10-03 12:24:36 -0700

    save critic output

--- local/experiments.sh
+++ local/experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/gens1000/D-Indirect-Step/config.yaml'
-python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/gens1000/D-Indirect-Traj/config.yaml'
-python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/gens1000/G/config.yaml'
-python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/gens1000/Fitness Critic/config.yaml'
-python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/gens1000/D/config.yaml'
-python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/gens1000/Alignment/config.yaml'
+python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/no_crash_influence/D-Indirect-Step/config.yaml'
+python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/no_crash_influence/Fitness Critic/config.yaml'
+python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/no_crash_influence/Alignment/config.yaml'
+python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/no_crash_influence/G/config.yaml'
+python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/no_crash_influence/D-Indirect-Traj/config.yaml'
+python ~/influence-multi-objective/influence_moo/influence_moo/run_cli.py '~/influence-multi-objective/results/simple_a/no_crash_influence/D/config.yaml'
